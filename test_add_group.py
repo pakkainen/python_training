7b7a14b07b4fe96291125a8d198b161a528eeb88
Ivan Pustovalov <ipustovalov@swordfishsecurity.com>
2023-02-21 15:51:38 +0300

    add contact creation test for Ex3

--- test_add_group.py
+++ test_add_group.py
@@ -13,20 +13,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="test group 1", header="test", footer="test"))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -34,10 +28,10 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
 
     def return_to_group_page(self, wd):
-        # return to groups page
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        self.open_group_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -52,13 +46,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_group_page(wd)
 
     def open_group_page(self, wd):
-        # open group page
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
-        # login
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -68,8 +62,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/index.php")
 
     def is_element_present(self, how, what):
         try:
