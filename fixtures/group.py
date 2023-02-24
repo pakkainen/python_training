f25c316687ac7ed7c9be2bea5735f092ef6560dd
Ivan Pustovalov <ipustovalov@swordfishsecurity.com>
2023-02-24 12:29:28 +0300

    add general fixture for Ex6

--- fixtures/group.py
+++ fixtures/group.py
@@ -26,6 +26,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
+    def delete_first(self):
+        wd = self.app.wd
+        self.open_group_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # delete first group
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
     def open_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
