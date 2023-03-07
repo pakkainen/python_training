f34705c51c89e05b0a1505c005e49b46f2fc0eca
Ivan Pustovalov <ipustovalov@swordfishsecurity.com>
2023-03-07 15:36:58 +0300

    add optimize page transitions for Ex10

--- fixtures/contact.py
+++ fixtures/contact.py
@@ -8,7 +8,8 @@ class ContactHelper:
 
     def open_add_contact_form(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def open_contacts_list(self):
         wd = self.app.wd
